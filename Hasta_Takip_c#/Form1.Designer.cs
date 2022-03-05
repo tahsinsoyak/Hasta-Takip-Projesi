45bdbd0a465ba213fd4445133cfd01321008d148
Tahsin Soyak <54424377+tahsinsoyak@users.noreply.github.com>
2022-03-05 20:45:59 +0300

    Add files via upload

---
+++ Hasta_Takip_c#/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Hasta_Takip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1_ac = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(89, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(101, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(21, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port:";
+            // 
+            // button1_ac
+            // 
+            this.button1_ac.Location = new System.Drawing.Point(89, 97);
+            this.button1_ac.Name = "button1_ac";
+            this.button1_ac.Size = new System.Drawing.Size(75, 23);
+            this.button1_ac.TabIndex = 2;
+            this.button1_ac.Text = "aç";
+            this.button1_ac.UseVisualStyleBackColor = true;
+            this.button1_ac.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "kapat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(265, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(314, 589);
+            this.listBox1.TabIndex = 6;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(89, 65);
+            this.maskedTextBox1.Mask = "99999999999";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(101, 20);
+            this.maskedTextBox1.TabIndex = 7;
+            this.maskedTextBox1.TextChanged += new System.EventHandler(this.maskedTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(21, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "TC:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(21, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Bağlantı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(86, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Oda";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(21, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Üst Sınır:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(21, 262);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Alt Sınır:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(141, 159);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Nabız";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(197, 159);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Ates";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(87, 227);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(87, 262);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 17;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(141, 227);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 18;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(141, 262);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 13);
+            this.label13.TabIndex = 19;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(197, 227);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(0, 13);
+            this.label14.TabIndex = 20;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(197, 262);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(0, 13);
+            this.label15.TabIndex = 21;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(141, 193);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(0, 13);
+            this.label16.TabIndex = 22;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(197, 193);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(0, 13);
+            this.label17.TabIndex = 23;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label18.Location = new System.Drawing.Point(21, 193);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(39, 13);
+            this.label18.TabIndex = 24;
+            this.label18.Text = "Anlık:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 616);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1_ac);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1_ac;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+    }
+}
+
---
+++ Hasta_Takip_c#/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using MySql.Data.MySqlClient;
+
+namespace Hasta_Takip
+{
+    public partial class Form1 : Form
+    {
+        private string data;
+        public List<string> veriler = new List<string>();
+        public string[] dot,dotx;
+        public string tarih;
+        public string room1 = "10";
+        public string room2 = "40";
+        public string pulse1 = "70";
+        public string pulse2 = "120";
+        public string ates1 = "15";
+        public string ates2 = "45";
+        public bool read_bool = true;
+        MySqlConnection conn= new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=patient");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1_ac.Enabled = false;
+            maskedTextBox1.Enabled = false;
+            string [] ports=SerialPort.GetPortNames();
+            foreach (string port in ports)
+                comboBox1.Items.Add(port);
+            serialPort1.DataReceived+=new SerialDataReceivedEventHandler(SerialPort1_DataReceived);
+        }
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            data = serialPort1.ReadLine();
+            this.Invoke(new EventHandler(displayData_event));
+        }
+        private void displayData_event(object sender, EventArgs e)
+        {
+            
+            veriler.Add(DateTime.Now.ToString() + " " + data);
+            listBox1.Items.Insert(0, DateTime.Now.ToString() + "       " + data);
+            //veri tabanı insert işilemi
+            //CREATE TABLE `hasta`.`veriler` ( `tarih` VARCHAR(25) NOT NULL , `tc` DOUBLE NOT NULL , `nabiz` INT NOT NULL , `ates` INT NOT NULL ) ENGINE = InnoDB;
+            //(char)i sayıyı byte'a çevirme
+            if (listBox1.Items.Count > 600)
+            {
+                for(int i = listBox1.Items.Count/2;i< listBox1.Items.Count;i++)
+                {
+                    listBox1.Items.RemoveAt(i);
+                }
+            }
+            if (veriler.Count > 5)
+            {
+                try
+                {
+                    insert(veriler);
+                    veriler.Clear();
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+
+            //5 gelen veride bir kere sınırları kontrol ediyoruz
+            if (veriler.Count % 1 == 0 || read_bool ==true)
+            {
+                //alarm gönderme
+                read_bool = true;
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM limits order by id desc LIMIT 1;", conn);
+                try
+                {
+                    conn.Open();
+                    MySqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        room1 = dr.GetString(1);
+                        room2 = dr.GetString(2);
+                        pulse1 = dr.GetString(3);
+                        pulse2 = dr.GetString(4);
+                        ates1 = dr.GetString(5);
+                        ates2 = dr.GetString(6);
+                    }
+                    conn.Close();
+                    read_bool = false;
+                }
+                catch (Exception)
+                {
+
+                }
+                   
+
+            }
+
+            label10.Text = room1;
+            label11.Text = room2;
+            label12.Text = pulse1;
+            label13.Text = pulse2;
+            label14.Text = ates1;
+            label15.Text = ates2;
+
+            dotx = data.Split(' ');
+            label16.Text = dotx[1];//nabız
+            label17.Text = dotx[2];//ates
+            label4.Text = dotx[3];//oda
+            if (Convert.ToInt32(dotx[1]) >Convert.ToInt32(pulse1) && Convert.ToInt32(dotx[1]) < Convert.ToInt32(pulse2) && Convert.ToInt32(dotx[2]) > Convert.ToInt32(ates1) && Convert.ToInt32(dotx[2]) < Convert.ToInt32(ates2) && Convert.ToInt32(dotx[3]) > Convert.ToInt32(room1) && Convert.ToInt32(dotx[3]) < Convert.ToInt32(room2))
+            {
+                serialPort1.Write("0");
+            }
+            else
+            {
+                if(Convert.ToInt32(dotx[1]) > Convert.ToInt32(pulse1) && Convert.ToInt32(dotx[1]) < Convert.ToInt32(pulse2) && Convert.ToInt32(dotx[2]) > Convert.ToInt32(ates1) && Convert.ToInt32(dotx[2]) < Convert.ToInt32(ates2))
+                    //bu kısım 2 olacak
+                    serialPort1.Write("2");//hemsireye sinyal ver oda sıcaklığı bozuk 
+                else
+                    serialPort1.Write("1");//hemsire ve doktora sinyal ver =oda sıcaklığıyla beraber ates veya nabız bozuk
+            }
+
+        }
+        private void insert(List<string> veri)
+        {
+            //veri.Reverse();
+            MySqlCommand cmd;
+            string query;
+            conn.Open();
+            foreach (string s in veri)
+            {
+                dot = s.Split(' ');
+                tarih = dot[0]+" "+ dot[1];//tarih ve saat için
+                query = "insert into datas (date, tc, nabiz, ates, sicaklik) values('" + tarih + "',"+maskedTextBox1.Text+" ,"+Convert.ToInt32(dot[3])+ ","+ Convert.ToInt32(dot[4]) + "," + Convert.ToInt32(dot[5]) + ");";
+                cmd = new MySqlCommand(query,conn);
+                cmd.ExecuteNonQuery();
+            }
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+                button2.Enabled = true;
+                button1_ac.Enabled = false;
+                maskedTextBox1.Enabled = false;
+                /*
+                int bip = Convert.ToInt32(label4.Text);
+                char cm=(char)bip;
+                label8.Text = cm.ToString();
+                Thread.Sleep(1000);
+                serialPort1.Write(cm.ToString());*/
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            button1_ac.Enabled = true;
+            button2.Enabled = false;
+            maskedTextBox1.Enabled = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (veriler.Count > 0)
+                insert(veriler);
+            if (serialPort1.IsOpen) 
+                serialPort1.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            maskedTextBox1.Enabled = true;
+        }
+
+        private void maskedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (maskedTextBox1.TextLength == 11 && Convert.ToDouble(maskedTextBox1.Text) > 10000000000)
+                {
+                    button1_ac.Enabled = true;
+                }
+                else
+                {
+                    button1_ac.Enabled = false;
+                }
+            }
+            catch (Exception)
+            {
+                maskedTextBox1.Clear();
+            }
+
+        }
+    }
+}
